2d44cf22c83648f6aa97e2806de1aaa29f8ac65d
Abhishek Srinivasan <abhisheksrinivasan5@gmail.com>
2023-12-31 15:06:15 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import ScrollReveal from './ScrollReveal.tsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScrollReveal>{}</ScrollReveal>
     </div>
   );
 }
---
+++ src/ScrollReveal.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useRef } from 'react';
+import { Container, ThemeProvider, CssBaseline } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
+
+const theme = createTheme();
+
+const useStyles = makeStyles(() => ({
+  reveal: {
+    opacity: 0,
+    transition: 'opacity 0.5s ease-in',
+  },
+  active: {
+    opacity: 1,
+  },
+  fadeReveal: {
+    transform: 'translateY(20px)',
+    animation: '$fadeRevealAnimation 0.7s ease-in',
+  },
+  '@keyframes fadeRevealAnimation': {
+    '0%': {
+      transform: 'translateY(0px)',
+      opacity: 0,
+    },
+    '100%': {
+      transform: 'translateY(0)',
+      opacity: 1,
+    },
+  },
+}));
+
+function ScrollReveal({ children }) {
+  const classes = useStyles();
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const element = ref.current;
+
+    const reveal = () => {
+      if (!element) {
+        return;
+      }
+
+      const windowHeight = window.innerHeight;
+      const top = element.getBoundingClientRect().top;
+      const activeClass = classes.active;
+
+      if (top < windowHeight - 150) {
+        element.classList.add(activeClass);
+      } else {
+        element.classList.remove(activeClass);
+      }
+    };
+
+    window.addEventListener('scroll', reveal);
+
+    reveal();
+
+    return () => window.removeEventListener('scroll', reveal);
+  }, [classes.active]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container>
+        <div ref={ref} className={`${classes.reveal} `}>
+          {children}
+        </div>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default ScrollReveal;
